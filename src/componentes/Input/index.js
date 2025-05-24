3c6460004b2d4eabed61048960a300be1c72b742
mizae zuza <mizael.zuza.estudos@gmail.com>
2025-05-23 21:49:47 -0300

    adição da sessão de pesquisa

---
+++ src/componentes/Input/index.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Input = styled.input`
+    width: 30%;
+    height: 45px;
+    background: transparent;
+    border: 1px solid #D5D5D5;
+    box-sizing: border-box;
+    border-radius: 15px;
+    margin: 0 auto;
+    padding: 0 10px;
+    color:rgb(173, 172, 172);
+    font-size: 16px;
+    text-align: center;
+`
+
+export default Input
